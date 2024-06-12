e9b8f469f7ff9e31c851c66cd4b444a8c5202390
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-06-12 01:18:52 -0400

    change some pun notifications
    removed a few unnecessary notifications, made player join/leave notifs much shorter and made them not play a sound

--- GorillaXS.Basics/PUNNotifications.cs
+++ GorillaXS.Basics/PUNNotifications.cs
@@ -7,29 +7,14 @@ namespace GorillaXS.Basics
 {
     internal class PUNNotifications : MonoBehaviourPunCallbacks
     {
-        public override void OnJoinedRoom()
-        {
-            Notifier.Notify("Room Joined", string.Format("You have joined room {0} with {1}/{2} players", PhotonNetwork.CurrentRoom.Name, PhotonNetwork.CurrentRoom.PlayerCount, PhotonNetwork.CurrentRoom.MaxPlayers));
-        }
-
-        public override void OnLeftRoom()
-        {
-            Notifier.Notify("Room Left", "You have left the room");
-        }
-
         public override void OnPlayerEnteredRoom(Player newPlayer)
         {
-            Notifier.Notify("Player Joined", string.Format("{0} has joined the room", PlayFabAuthenticator.instance.GetSafety() ? newPlayer.DefaultName : newPlayer.NickName));
+            Notifier.Notify("Player Joined", string.Format("{0} has joined the room", PlayFabAuthenticator.instance.GetSafety() ? newPlayer.DefaultName : newPlayer.NickName), timeout: 1.5f, AudioPath: "");
         }
 
         public override void OnPlayerLeftRoom(Player otherPlayer)
         {
-            Notifier.Notify("Player Left", string.Format("{0} has left the room", PlayFabAuthenticator.instance.GetSafety() ? otherPlayer.DefaultName : otherPlayer.NickName));
-        }
-
-        public override void OnMasterClientSwitched(Player newMasterClient)
-        {
-            Notifier.Notify("Room Update", string.Format("The master client has been switched to {0}", PlayFabAuthenticator.instance.GetSafety() ? newMasterClient.DefaultName : newMasterClient.NickName), Base64Icon: "warning");
+            Notifier.Notify("Player Left", string.Format("{0} has left the room", PlayFabAuthenticator.instance.GetSafety() ? otherPlayer.DefaultName : otherPlayer.NickName), timeout: 1.5f, AudioPath: "");
         }
 
         public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
